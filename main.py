47934ca2b3cd01e7201eafdd3ca649924eff5a68
Артём <samakata93@gmail.com>
2023-03-21 19:08:53 +0300

    startPush

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print('csv manager')
+import os
+import file
+import list.function
+import yi
+def main():
+    os.system('cls')
+    yi.show_menu()
+    try:
+        choice = yi.choice_menu()
+        print(choice)
+        if choice==7:
+            yi.show_data(file.read_file('data.csv'))
+            input()
+            main()
+    except Exception as ex:
+        print(str(ex))
+        input()
+        main()
+
+print("start program")
+main()
+
